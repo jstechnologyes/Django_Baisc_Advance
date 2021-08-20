48ee07ec9986b4f2d54b5bc44e1c5c24f681b971
Hadiuzzaman <hadi.mmpi@gmail.com>
2021-08-20 20:42:02 +0600

    Post Method Added

--- DjangoBasicToAdvance/views.py
+++ DjangoBasicToAdvance/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import HttpResponse, render
 def home(request):
-    name=['Hadi','Sadi','Joti']
+    if request.method=="GET":
+        name=['Hadi','Sadi','Joti']
+    else:
+        name=[]
     context={
         'name':name,
     }
     return render(request,'home.html',context)
+
+def contact(request):
+    if request.method=="POST":
+        name=request.POST['name']
+        phone=request.POST['phone']
+        content=request.POST['content']
+        print(name)
+        print(phone)
+        print(content)
+    return render(request,'contact.html')
